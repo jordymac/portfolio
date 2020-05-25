9a323dc471418b156f9b1e99dbec836066b03bd6
Jordy <61567660+jordymac@users.noreply.github.com>
2020-05-26 08:51:42 +1000

    Add files via upload

---
+++ assets/js/nav.js
@@ -0,0 +1,8 @@
+const toggleCollapse = document.querySelector('.toggle-collapse span');
+const nav = document.querySelector('.nav');
+
+
+// onclick event on toggle Collapse span tag
+toggleCollapse.onclick = () =>{
+    nav.classList.toggle('collapse');
+}
